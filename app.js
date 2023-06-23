f26c32ab5c23b271c1bb0a7df70cb0a66ea70e19
Sage <blazemacroz@gmail.com>
2023-06-22 20:30:18 -0500

    worked on express server

--- app.js
+++ app.js
@@ -1,7 +1,109 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const posts = postBank.find(id);
+
+  if (!posts.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else{
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${posts.id}. ▲</span>${posts.title}
+            <small>(by ${posts.name})</small>
+          </p>
+          <p>
+          <span class="news-position">${posts.content}</span>
+          </p>
+          <small class="news-info">
+            ${posts.upvotes} upvotes | ${posts.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+  res.send(html)
+}});
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html =`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(posts => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${posts.id}. ▲</span>
+            ${posts.title}
+            <small>(by ${posts.name})</small>
+          </p>
+          <small class="news-info">
+            ${posts.upvotes} upvotes | ${posts.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+  res.send(html);
+});
+
+app.use((req, res)=>{
+  res.status(404).send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+  )
+})
 
 const PORT = 1337;
 
